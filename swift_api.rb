1e580aaa0d108a3df885eeb8c54366ed4b49f76a
Evgenia Manolova <emanolova@gmail.com>
2015-10-17 08:22:46 +0300

    use iframes and add group2

--- swift_api.rb
+++ swift_api.rb
@@ -22,6 +22,8 @@ namespace '/api' do
 
     if Dir.exists?(dir)
       entries = Dir.entries(dir).reject {|x| x[0] == '.'}
+      
+      #      entries.reject! { /\.html\z/ } if %w(group1 group2).include?(dir) # Doesn't work
 
       unless entries.empty?
         result = "<ul class=\"jqueryFileTree\" style=\"display: none;\">"
@@ -29,10 +31,12 @@ namespace '/api' do
         files = ""
 
         entries.each do |file|
+          rel = "#{dir}/#{file}"
+          
           if Dir.exists?(File.join(dir,file))
-            dirs += "<li class=\"directory collapsed\"><a href=\"#\" rel=\"#{dir}/#{file}/\">#{file}</a></li>"
+            dirs += "<li class=\"directory collapsed\"><a href=\"#\" rel=\"/#{rel}\">#{file}</a></li>"
           else
-            files += "<li class=\"file ext_#{File.extname(file).sub(/^\./,'')}\"><a href=\"#\" rel=\"/#{dir}/#{file}\">#{file}</a></li>"
+            files += "<li class=\"file ext_#{File.extname(file).sub(/\a\./,'')}\"><a href=\"#\" rel=\"/#{rel}\">#{file}</a></li>"
           end
         end
 
