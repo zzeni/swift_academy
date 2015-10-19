dc0614ae4024da509dbe7942f90b756372c87020
Evgenia Manolova <emanolova@gmail.com>
2015-10-19 23:48:38 +0300

    lessons functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use_strict';      
+      
 $(document).on("ready", function() {
   $(window).on('scroll', function(e) {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop;
@@ -9,5 +11,13 @@ $(document).on("ready", function() {
       if ($('header').hasClass('collapse'))
         $('header').removeClass('collapse');
     }
+  });
+  
+  $('.lesson-preview a').click(function(e) {
+    var lesson = $(this).attr('data-target');
+    var lesson_file = lesson.charAt(0).toUpperCase() + lesson.slice(1) + '.pdf';
+    $('#presentation').attr('src', 'http://89.190.215.198/lessons/' + lesson + '/' + lesson_file);
+    $('#examples').attr('src', 'http://89.190.215.198/lessons/' + lesson + '/example/index.html');
+    e.preventDefault();
   });
 });
