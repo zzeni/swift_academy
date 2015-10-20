4ec2feed04e57bdcff0df0ffc9841c098d415d87
Evgenia Manolova <emanolova@gmail.com>
2015-10-20 06:45:07 +0300

    improve lessons

--- js/main.js
+++ js/main.js
@@ -15,8 +15,18 @@ $(document).on("ready", function() {
   
   $('.lesson-preview a').click(function(e) {
     var lesson = $(this).attr('data-target');
-    $('#presentation').attr('src', 'http://courses.zenifytheweb.com/lessons/' + lesson + '/lesson.pdf');
-    $('#examples').attr('src', 'http://courses.zenifytheweb.com/lessons/' + lesson + '/example/index.html');
+    var lesson_index =  lesson.charAt(lesson.length - 1);
+    var lesson_link = 'http://courses.zenifytheweb.com/lessons/' + lesson + '/lesson.pdf';
+    var example_link = 'http://courses.zenifytheweb.com/lessons/' + lesson + '/example/index.html';
+    var homework_link = 'https://github.com/zzeni/swift-academy-homeworks/tree/master/L' + lesson_index;
+    $('.lesson-preview .selected').removeClass('selected');
+    $(this).parent().addClass('selected');
+    $('#lesson-name').text('Lesson ' + lesson_index);
+    $('#presentation').attr('src', lesson_link);
+    $('#presentation').parent().find('a:first').attr('href', lesson_link);
+    $('#examples').attr('src', example_link);
+    $('#examples').parent().find('a:first').attr('href', example_link);
+    $('#homework-viewer').find('a:first').attr('href', homework_link).text(homework_link);
     e.preventDefault();
   });
 });
