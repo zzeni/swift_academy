51235dbb887e191f06973de8300ffd3c34713cc5
Evgenia Manolova <emanolova@gmail.com>
2015-11-13 00:11:34 +0200

    fix the lesson number

--- js/lessons.js
+++ js/lessons.js
@@ -1,10 +1,10 @@
-'use_strict';      
-      
+'use_strict';
+
 $(document).on("ready", function() {
-  
+
   $('.lesson-preview a').click(function(e) {
     var lesson = $(this).attr('data-target');
-    var lesson_index =  lesson.charAt(lesson.length - 1);
+    var lesson_index =  lesson.split("lesson")[1];
     var lesson_link = 'http://courses.zenifytheweb.com/lessons/' + lesson + '/lesson.pdf';
     var example_link = 'http://courses.zenifytheweb.com/lessons/' + lesson + '/example/index.html';
     var homework_link = 'https://github.com/zzeni/swift-academy-homeworks/tree/master/tasks/L' + lesson_index;
@@ -18,9 +18,9 @@ $(document).on("ready", function() {
     $('#homework-viewer').find('a:first').attr('href', homework_link).text(homework_link);
     e.preventDefault();
   });
-  
+
   $('.lesson-preview a:last-child').click();
-  
+
   if ($('.lesson-preview ul').width() > $('.lesson-preview').width()) {
     var diff = $('.lesson-preview ul').width() - $('.lesson-preview').width() + 22;
     $(".lesson-preview").animate({ scrollLeft: diff }, 2000);
