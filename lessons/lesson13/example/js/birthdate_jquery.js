4550a84a85fc6f88be8de4dedf80990dbbd68b81
Evgenia Manolova <emanolova@gmail.com>
2015-11-22 21:48:12 +0200

    add lesson 13

---
+++ lessons/lesson13/example/js/birthdate_jquery.js
@@ -0,0 +1,24 @@
+"use strict";
+
+$(function () {
+  console.log('ready');
+
+  $('#addPerson').submit(function(e) {
+    e.preventDefault();
+    console.log('form submitted..');
+
+    var person = new Person($('#nameInput').val(), $('#birthdateInput').val());
+
+    var nameContainer = $('<h4 class="person-hearer">' + person.name + '</h4>');
+    var infoContainer = $('<div class="info-container">' + person.toString() + '</div>');
+    var personContainer = $('<li class="person-container"></li>');
+
+    personContainer.append(nameContainer);
+    personContainer.append(infoContainer);
+
+    $("#people").append(personContainer);
+  });
+
+  $("#people").sortable();
+  $("#people").disableSelection();
+});
