07c73ad7462cdf104466f63e6257c04061038fc1
Evgenia Manolova <emanolova@gmail.com>
2015-11-28 14:10:40 +0200

    re-format projects

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 'use_strict';
 
-$(document).on("ready", function() {
-  $(window).on('scroll', function(e) {
+$(document).on("ready", function () {
+  $(window).on('scroll', function (e) {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop;
     if (distanceY > 150) {
       if (!$('header').hasClass('collapse'))
         $('header').addClass('collapse');
-    }
-    else {
+        $('aside').addClass('expand');
+    } else {
       if ($('header').hasClass('collapse'))
         $('header').removeClass('collapse');
+        $('aside').removeClass('expand');
     }
   });
 
-  $(window).on('resize', function() {
+  $(window).on('resize', function () {
 
-    $('#item-preview').each(function() {
+    $('#item-preview').each(function () {
       var width = $(this).width();
-      $(this).find('iframe').height(width*0.7);
+      $(this).find('iframe').height(width * 0.7);
     });
 
-      var min_height = $(window).height() - $('footer').outerHeight() - $('header').outerHeight();
+    var min_height = $(window).height() - $('footer').outerHeight() - $('header').outerHeight();
 
-      if (min_height > 0) {
-        $('main').css('min-height', '' + min_height + 'px');
-      }
+    if (min_height > 0) {
+      $('main').css('min-height', '' + min_height + 'px');
+    }
   });
 
   $(window).resize(); // trigger the resize event in order to set footer position
