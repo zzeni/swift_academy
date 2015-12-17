c80c36d4373ff81065a79a0bf14769461e3d324c
Evgenia Manolova <emanolova@gmail.com>
2015-12-17 15:29:32 +0200

    add lesson 20

---
+++ lessons/lesson20/example/database.js
@@ -0,0 +1,23 @@
+var ITEMS_DB = [
+  {
+    name: "A flower",
+    price: "5",
+    colour: "yellow",
+    description: "My first flower",
+    image_url: "http://www.tattoomenow.com/tattoo-designs/wp-content/uploads/2012/05/flower-300x300.jpg"
+  },
+  {
+    name: "Flower 2",
+    price: "15",
+    colour: "red",
+    description: "some other flower",
+    image_url: "http://blog.meetthegimp.org/wp-content/uploads/2008/12/flower.jpg"
+  },
+  {
+    name: "Another flower",
+    price: "8.5",
+    colour: "purple",
+    description: "Yet another flower",
+    image_url: "http://www.thegardenswap.com/articleImages/FlowerGardening.jpg"
+  },
+];
