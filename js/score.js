0f8c1b9037634ff14e803983f17602d02d4e2c67
Evgenia Manolova <emanolova@gmail.com>
2016-01-12 15:09:29 +0200

    update group2 scores

--- js/score.js
+++ js/score.js
@@ -1,12 +1,12 @@
-function processScore(group,format) {
+function processScore(group, format) {
   "use strict";
 
   if (!format.match(/^homeworks|project|final$/)) console.error("wrong format: " + format);
 
-  var scoresFunc = function scoresFunc(scores,format) {
+  var scoresFunc = function scoresFunc(scores, format) {
     console.log(scores);
 
-    $.each(scores,function(index,score) {
+    $.each(scores, function (index, score) {
       var hw_total = 0;
       var exam_total = 0;
       var project_total = 0;
@@ -17,9 +17,9 @@ function processScore(group,format) {
       row.append('<td class="s4">' + index + '</td>');
       row.append('<td class="s3">' + score.name + '</td>');
 
-      for(var j=0;j<3;j++) {
+      for (var j = 0; j < 3; j++) {
         var sum = 0;
-        for(var i=(6*j)+1;i<=(6*j)+6;i++) {
+        for (var i = (6 * j) + 1; i <= (6 * j) + 6; i++) {
           var sc = score['l' + i];
           if (format === "homeworks") {
             row.append('<td class="s2">' + sc + '</td>');
@@ -28,25 +28,23 @@ function processScore(group,format) {
         }
         hw_total += sum;
         if (format === "homeworks" || format === "final") {
-          var style = (format==="homeworks")? 's8' : 's2';
+          var style = (format === "homeworks") ? 's8' : 's2';
           row.append('<td class="' + style + '">' + sum + '</td>');
         }
       }
 
       if (format === "homeworks" || format === "final") {
-        var style = (format==="homeworks")? 's2' : 's8';
+        var style = (format === "homeworks") ? 's2' : 's8';
         if (hw_total <= 100) {
           row.append('<td class="' + style + ' danger">' + hw_total + '</td>');
-        }
-        else if (hw_total < 260) {
+        } else if (hw_total < 260) {
           row.append('<td class="' + style + ' warning">' + hw_total + '</td>');
-        }
-        else {
+        } else {
           row.append('<td class="' + style + '">' + hw_total + '</td>');
         }
       }
 
-      for(var i=1; i<=4; i++) {
+      for (var i = 1; i <= 4; i++) {
         if (format === "final") {
           row.append('<td class="s2">' + score['test' + i] + '</td>');
         }
@@ -63,13 +61,12 @@ function processScore(group,format) {
         "proj_code",
         "proj_use",
         "proj_complete"
-      ],function(index,key_name) {
+      ], function (index, key_name) {
         if (format === "project") {
-          var comment = score[key_name + '_comment']
+          var comment = score[key_name + '_comment'];
           if (comment === undefined) {
             row.append('<td class="s2">' + score[key_name] + '</td>');
-          }
-          else {
+          } else {
             row.append('<td class="s2 commented" title="' + comment + '">' + score[key_name] + '</td>');
           }
         }
@@ -83,8 +80,8 @@ function processScore(group,format) {
       // Now all the final scores
       if (format === "final") {
         // persistance
-        var persistence = Math.round((Math.max(hw_total-100,0))/20);
-        var exam = Math.round((exam_total + project_total*5)/90);
+        var persistence = Math.round((Math.max(hw_total - 100, 0)) / 20);
+        var exam = Math.round((exam_total + project_total * 5) / 90);
         var bonus = 0;
 
         if (persistence > 10) {
@@ -100,13 +97,16 @@ function processScore(group,format) {
         // exam
         row.append('<td class="s8">' + exam + '/10</td>');
         // teacher's grade
-        row.append('<td class="s8">?/10</td>');
-//        row.append('<td class="s8">' + score.recommendation + '/10</td>');
+        if (window.location.hostname === "127.0.0.1") {
+          var commented = (score.recommendation_comment !== undefined)? "commented" : "";
+          row.append('<td class="s8 ' + commented + '" title="' + score.recommendation_comment +'">' + score.recommendation + '/10</td>');
+        } else {
+          row.append('<td class="s8">?/10</td>')
+        }
         // binus points
         if (bonus > 0) {
           row.append('<td class="s8 bonus">' + bonus + '</td>');
-        }
-        else {
+        } else {
           row.append('<td class="s8">-</td>');
         }
       }
@@ -115,13 +115,15 @@ function processScore(group,format) {
     });
   }
 
-  var jsonReq = $.getJSON("../js/scores/" + group + ".json", function(data) {
-    var score = data["score"];
-    scoresFunc(score,format);
-  })
-  .fail(function( jqxhr, textStatus, error ) {
-    var err = textStatus + ", " + error;
-    console.log( "Request Failed: " + err );
-  })
-  .done(function(){ console.log('success'); });
+  var jsonReq = $.getJSON("../js/scores/" + group + ".json", function (data) {
+      var score = data["score"];
+      scoresFunc(score, format);
+    })
+    .fail(function (jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      console.log("Request Failed: " + err);
+    })
+    .done(function () {
+      console.log('success');
+    });
 }
