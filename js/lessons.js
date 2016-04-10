a58887230cc392a2f360b9f15f94db8347ee5783
Evgenia Manolova <emanolova@gmail.com>
2016-04-10 23:10:05 +0300

    add lesson 6

--- js/lessons.js
+++ js/lessons.js
@@ -1,7 +1,7 @@
 'use_strict';
 
 $(document).on("ready", function() {
-  var version = "0.2"
+  var version = "0.3"
 
   $('.lesson-preview a').click(function(e) {
     var lesson = $(this).attr('data-target');
