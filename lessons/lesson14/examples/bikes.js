fbf0c444d18bc361b590615af75b9d818098258f
Evgenia Manolova <emanolova@gmail.com>
2016-05-12 13:30:04 +0300

    add test 2 scores and L14 examples

---
+++ lessons/lesson14/examples/bikes.js
@@ -0,0 +1,60 @@
+var bikes = [
+  {
+    name: "Drag",
+    price: 200
+  },
+  {
+    name: "RAM",
+    price: 500
+  },
+  {
+    name: "Scott",
+    price: 1200
+  }
+];
+
+console.log("Need a bike?..");
+
+function logBikes(bukesArray) {
+  bukesArray.forEach(function (bike) {
+    console.log(bike.name + ": " + bike.price);
+  });
+}
+
+logBikes(bikes);
+
+// ===========================================
+
+console.log("I just wanna know who are you..");
+
+bikes.whoAmI = function() {
+  console.log(this);
+};
+
+bikes.whoAmI();
+
+// ===========================================
+
+bikes.customForEach = function(callback) {
+  for (var i=0, size = this.length; i < size; i++) {
+    callback(this[i]);
+  }
+};
+
+console.log("The result from the custom forEach is: ");
+
+bikes.customForEach(function (bike) {
+  console.log(bike.name + ": " + bike.price);
+});
+
+// ===========================================
+
+console.log("Let's filter the bikes by price!");
+
+var filteredBikes = bikes.filter(function (bike) {
+  return bike.price < 1000;
+});
+
+logBikes(filteredBikes);
+
+
