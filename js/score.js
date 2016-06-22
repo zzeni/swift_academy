5ab09906c3c5e61567d2fbe61da82ca156ef97b0
Evgenia Manolova <emanolova@gmail.com>
2016-06-22 13:41:45 +0300

    certificate scores

--- js/score.js
+++ js/score.js
@@ -7,145 +7,116 @@ function processScore(group, format) {
     console.log(scores);
 
     $.each(scores, function (index, score) {
-      var hw_total = 0;
-      var exam_total = 0;
-      var project_total = 0;
-
       var table = $('#score');
       var row = $('<tr></tr>');
 
       row.append('<td class="s4">' + index + '</td>');
       row.append('<td class="s3">' + score.name + '</td>');
 
-      for (var j = 0; j < 3; j++) {
-        var sum = 0;
-        for (var i = (6 * j) + 1; i <= (6 * j) + 6; i++) {
-          var sc = score['l' + i];
-          if (format === "homeworks") {
-            row.append('<td class="s2">' + sc + '</td>');
-          }
-          sum += sc;
-        }
-        hw_total += sum;
-//        if (format === "homeworks" || format === "final") {
-        if (format === "homeworks") {
-          var style = (format === "homeworks") ? 's8' : 's2';
-          row.append('<td class="' + style + '">' + sum + '</td>');
-        }
-      }
+      score.test1.total = score.test1.raw[0] + score.test1.raw[1];
+      score.test1.score = Math.round(score.test1.total / 2.65) + score.test1.raw[2];
 
-      if (format === "homeworks" || format === "final") {
-        var style = (format === "homeworks") ? 's2' : 's8';
-        if (hw_total <= 100) {
-          row.append('<td class="' + style + ' danger">' + hw_total + '</td>');
-        } else if (hw_total < 160) {
-          row.append('<td class="' + style + ' warning">' + hw_total + '</td>');
-        } else {
-          row.append('<td class="' + style + '">' + hw_total + '</td>');
-        }
-      }
+      score.test2.total = score.test2.part1 +
+        score.test2.part2 +
+        score.test2.part3_1 +
+        score.test2.part3_2 +
+        score.test2.part3_3;
+      score.test2.score = Math.round(score.test2.total * 0.4);
 
-      for (var i = 1; i <= 4; i++) {
-        if (format === "final") {
-          row.append('<td class="s2">' + score['test' + i] + '</td>');
-        }
-        exam_total += score['test' + i];
-      }
+      score.test4.total = score.test4.test + score.test4.css + score.test4.js;
+      score.test4.score = Math.round(score.test4.total / 2.5);
 
-      $.each([
-        "proj_layout",
-        "proj_design",
-        "proj_navigation",
-        "proj_responsiveness",
-        "proj_js",
-        "proj_effects",
-        "proj_code",
-        "proj_use",
-        "proj_complete"
-      ], function (index, key_name) {
-        if (format === "project") {
-          var comment = score[key_name + '_comment'];
-          if (comment === undefined) {
-            row.append('<td class="s2">' + score[key_name] + '</td>');
-          } else {
-            row.append('<td class="s2 commented" title="' + comment + '">' + score[key_name] + '</td>');
-          }
-        }
-        project_total += score[key_name];
-      });
+      score.examsTotal = score.test1.score +
+        score.test2.score +
+        score.test3.score +
+        score.test4.score / 2;
 
-      if (format === "project" || format === "final") {
-        row.append('<td class="s2">' + project_total + '</td>');
-      }
-
-      // Now all the final scores
-      if (format === "final") {
-        // persistance
-        var persistence = Math.round((hw_total + 10) / 20);
-        var exam = Math.round(exam_total / 10);
-        var bonus = 0;
-
-        if (persistence > 10) {
-          bonus += persistence - 10;
-          persistence = 10;
+      score.homeworks.array = [];
+      score.homeworks.total = 0;
+      for (var key in score.homeworks) {
+        if (key.match(/l\d+/)) {
+          score.homeworks.array.push(score.homeworks[key]);
+          score.homeworks.total += score.homeworks[key];
         }
-        if (exam > 10) {
-          bonus += exam - 10;
-          exam = 10;
-        }
+      };
 
-        row.append('<td class="s8">' + persistence + '/10</td>');
-        // exam
-        row.append('<td class="s8">' + exam + '/10</td>');
-        // teacher's grade
-        if (window.location.hostname === "127.0.0.1") {
-          var commented = (score.recommendation_comment !== undefined)? "commented" : "";
-          row.append('<td class="s8 ' + commented + '" title="' + score.recommendation_comment +'">' + score.recommendation + '/10</td>');
-        } else {
-          row.append('<td class="s8">?/10</td>')
-        }
-        // binus points
-//        if (bonus > 0) {
-//          row.append('<td class="s8 bonus">' + bonus + '</td>');
-//        } else {
-//          row.append('<td class="s8">-</td>');
-//        }
-      }
+      score.homeworks.total = score.homeworks.total / 2;
+
+      score.projectTotal = 0;
+
+      score.certificate = Math.round((score.examsTotal + score.homeworks.total + score.projectTotal) / 3);
 
       if (format === "test1") {
-        var arr = score["test1_arr"];
-        var length = 5;
-        for(i=0; i<length; i++) {
-          var value = arr[i] || "";
-          if (i>=length-2)
-            row.append('<td class="s8">' + value + '</td>');
-          else
-            row.append('<td class="s2">' + value + '</td>');
-        }
+        row.append('<td class="s2">' + score.test1.raw[0] + '</td>');
+        row.append('<td class="s2">' + score.test1.raw[1] + '</td>');
+        row.append('<td class="s8">' + score.test1.total + '</td>');
+        row.append('<td class="s8">' + score.test1.raw[2] + '</td>');
+        row.append('<td class="s6">' + score.test1.score + '</td>');
+      } else if (format === "test2") {
+        row.append('<td class="s2">' + score.test2.part1 + '</td>');
+        row.append('<td class="s2">' + score.test2.part2 + '</td>');
+        row.append('<td class="s2">' + score.test2.part3_1 + '</td>');
+        row.append('<td class="s2">' + score.test2.part3_2 + '</td>');
+        row.append('<td class="s2">' + score.test2.part3_3 + '</td>');
+        row.append('<td class="s8">' + score.test2.total + '</td>');
+        row.append('<td class="s8">' + score.test2.score + '</td>');
+      } else if (format === "test3") {
+        row.append('<td class="s8">' + score.test3.score + '</td>');
+      } else if (format === "test4") {
+        row.append('<td class="s2">' + score.test4.test + '</td>');
+        row.append('<td class="s2">' + score.test4.css + '</td>');
+        row.append('<td class="s2">' + score.test4.js + '</td>');
+        row.append('<td class="s2">' + score.test4.total + '</td>');
+        row.append('<td class="s8">' + score.test4.score + '</td>');
+      } else if (format === "homeworks") {
+        score.homeworks.array.forEach(function (item) {
+          row.append('<td class="s2">' + item / 10 + '</td>');
+        });
+        row.append('<td class="s8">' + score.homeworks.total + '%</td>');
+      } else if (format === "final") {
+//        if (score.certificate < 35 || score.test4.total === 0 || score.projectTotal === 0)
+        if (score.test4.total === 0)
+          row.addClass("danger");
+//        else if (score.certificate < 50)
+//          row.addClass("warning");
+
+        row.append('<td class="s2">' + score.test1.score + '</td>');
+        row.append('<td class="s2">' + score.test2.score + '</td>');
+        row.append('<td class="s2">' + score.test3.score + '</td>');
+        row.append('<td class="s2">' + score.test4.score + '</td>');
+        row.append('<td class="s8">' + score.homeworks.total + '%</td>');
+        row.append('<td class="s8">' + score.examsTotal + '%</td>');
+        row.append('<td class="s8">' + score.projectTotal + '%</td>');
+        row.append('<td class="s6">' + score.certificate + '%</td>');
       }
 
-      if (format === "test2") {
-        var testScores = score["test2_detail"];
-        var total = 0;
-        for (var part in testScores) {
-          total += testScores[part];
-          row.append('<td class="s2">' + testScores[part] + '</td>');
-        }        
-        row.append('<td class="s8">' + total + '</td>');
-        row.append('<td class="s8">' + Math.round(total * 0.4) + '</td>');
-      }
-      
-      if (format === "test3") {
-        row.append('<td class="s8">' + score["test3"] + '</td>');
-      }
-      
-      if (format === "test4") {
-        row.append('<td class="s2">0</td>');
-        row.append('<td class="s2">0</td>');
-        row.append('<td class="s2">0</td>');
-        row.append('<td class="s8">0</td>');
-      }
-      
+      //      $.each([
+      //        "proj_layout",
+      //        "proj_design",
+      //        "proj_navigation",
+      //        "proj_responsiveness",
+      //        "proj_js",
+      //        "proj_effects",
+      //        "proj_code",
+      //        "proj_use",
+      //        "proj_complete"
+      //      ], function (index, key_name) {
+      //        if (format === "project") {
+      //          var comment = score[key_name + '_comment'];
+      //          if (comment === undefined) {
+      //            row.append('<td class="s2">' + score[key_name] + '</td>');
+      //          } else {
+      //            row.append('<td class="s2 commented" title="' + comment + '">' + score[key_name] + '</td>');
+      //          }
+      //        }
+      //        project_total += score[key_name];
+      //      });
+      //
+      //      if (format === "project" || format === "final") {
+      //        row.append('<td class="s2">' + project_total + '</td>');
+      //      }
+      //
+
       table.append(row);
     });
   }
