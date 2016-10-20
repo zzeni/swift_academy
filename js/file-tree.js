58915988ed696170812e438cf0c4aabcf1a5210e
Evgenia Manolova <emanolova@gmail.com>
2016-10-20 16:02:23 +0300

    add lesson 6

--- js/file-tree.js
+++ js/file-tree.js
@@ -1,23 +1,23 @@
+'use_strict';
 
-'use_strict';      
-      
-$(document).on("ready", function() {
-  
-  $('#fileTree').each( function() {
+$(document).on("ready", function () {
+
+  $('#fileTree').each(function () {
     var target = $(this).attr("data-target");
-    
+
     if (window.location.hostname == "") {
       $('#fileTree-container').html('<iframe src="http://zenlabs.pro/courses/' + target + '/filetree.html"></iframe>')
-    }
-    else {
-      $('#fileTree').fileTree({ root: target, script: "/api/fs_read" }, function(file) { 
+    } else {
+      $('#fileTree').fileTree({
+        root: target,
+        script: "http://127.0.0.1:4567/api/fs_read"
+      }, function (file) {
         var url = '/courses/' + file;
-        
+
         if ($('#item-preview').size() > 0) {
           $('#item-preview').find('iframe').attr('src', url);
           $('#item-preview').find('a').attr('href', url);
-        }
-        else {
+        } else {
           window.location = url;
         }
       });
